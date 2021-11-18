7f3ae6dc87e84c8b75c4eb29318c9416527d561d
Derek Widmer <dwidmer99@gmail.com>
2021-11-18 00:26:20 -0500

    Basic, unworking auth. Gets code but error getting token

--- App.js
+++ App.js
@@ -1,15 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import SignIn from './components/signIn';
+import { useAuthRequest } from 'expo-auth-session';
+import axios from 'axios';
 
+const CLIENT_ID = '3f260525a6374905ab85be85aa9b1242';
+
+const config = {
+  clientId: CLIENT_ID,
+  redirectUri: 'exp://127.0.0.1:19000',
+  scopes: ['user-read-playback-position', 'user-read-recently-played', 'user-top-read'],
+};
+
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
 
 export default function App() {
+
+  const [request, response, promptAsync] = useAuthRequest(config, discovery);
+
+
+  useEffect(() => {
+    if (response) {
+      const { code } = response.params;
+      const codeVerifier = request.codeVerifier;
+      getTokens(code, codeVerifier)
+    }
+  }, [response])
+
+  const getTokens = async (code, codeVerifier) => {
+    axios.request(discovery.tokenEndpoint, {
+      method: "POST",
+      params: {
+        grant_type: "authorization_code",
+        code,
+        redirectUri: request.redirectUri,
+        clientId: CLIENT_ID,
+        code_verifier: codeVerifier
+      }
+    })
+      .then(res => console.log(res))
+      .catch(e => console.log('Error!: ', e))
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <SignIn />
+      <SignIn onPress={promptAsync} />
     </View>
   );
 }
--- components/signIn/index.js
+++ components/signIn/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { View, Text, Pressable } from 'react-native'
 import styles from './styles'
 
-export default function SignIn() {
+export default function SignIn(props) {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Welcome to BetterWrapped.</Text>
-			<Pressable style={styles.signIn}>
+			{/* <Text style={{ color: "white" }}>code: {props.response ? props.response.params.code : " "}</Text> */}
+			<Pressable style={styles.signIn} onPress={props.onPress}>
 				<Text style={styles.buttonText}>Sign in with Spotify</Text>
 			</Pressable>
 		</View>

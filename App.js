1c08ce005af68e5a839c664eb259080b45aebba3
Derek Widmer <dwidmer99@gmail.com>
2021-11-22 13:59:41 -0500

    GetRefreshedToken function working

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import SignIn from './components/signIn';
-
 import axios from 'axios';
 import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+
+const query = require('query-string')
 
 export default function App() {
 
@@ -14,52 +15,27 @@ export default function App() {
     tokenEndpoint: 'https://accounts.spotify.com/api/token',
   };
 
-  // state to store credentials
-  const [creds, setCredentials] = useState({})
-  const [tokens, setTokens] = useState({})
+  const CLIENT_ID = "3f260525a6374905ab85be85aa9b1242"
+
+  const [accessToken, setAccessToken] = useState("")
+  const [refreshToken, setRefreshToken] = useState("")
+
+
 
   // from AuthSession documentation Spotify example
   // hook meant for getting code
   const [request, response, promptAsync] = useAuthRequest(
     {
-      clientId: creds['clientId'],
+      clientId: CLIENT_ID,
       scopes: ['user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-state', 'user-library-modify',
         'user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public',
         'playlist-modify-private', 'user-read-recently-played', 'user-top-read'],
-      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
-      // this must be set to false
-      usePKCE: false,
+      usePKCE: true,
       redirectUri: makeRedirectUri(),
       show_dialog: true
     },
     discovery
   );
-
-  // When app loads, grab credentials from backend
-  useEffect(() => {
-    getCredentials();
-  }, []);
-
-  // useEffect(() => {
-  //   if (response) {
-  //     console.log(response)
-  //   }
-  // }, [response])
-
-  // Grab secret credentials from backend
-  const getCredentials = async () => {
-    axios({
-      method: 'GET',
-      url: 'http://localhost:3000/getCredentials'
-    })
-      // saving into state for use in API calls
-      .then(res => {
-        setCredentials(res.data);;
-        console.log('Set creds to ', res.data)
-      })
-      // error
-      .catch(e => { console.log("Error:", e) })
-  }
 
   // Getting code from authentication
   const getCode = async () => {
@@ -72,71 +48,48 @@ export default function App() {
           // Check if state is the same for security
           if (res.params.state === request.state) {
             // make request for tokens
-            getTokens(res.params.code);
+            // console.log("Got code: ", res.params.code, ". \nMaking request")
+            getTokens(res.params.code, request.codeVerifier);
           }
         }
-
       })
       // error in call
       .catch(err => console.log('Error: ', err))
   }
 
-  const getTokens = async (code) => {
-    try {
-      const config = {
-        method: 'POST',
-        headers: {
-          'Authorization': `Basic ${creds["credsB64"]}`,
-        },
-        body: {
-          "grant_type": 'authorization_code',
-          "code": code,
-          "redirect_uri": creds["redirectUri"]
-        },
-        mode: 'cors'
-      }
-      // console.log("Sending request with: ", config)
-      axios.post('http://127.0.0.1:3000/getTokens', config)
-        // fetch('http://127.0.0.1:3000/getTokens', config)
-        .then(res => console.log(res.status, res.statusText))
-        .catch(e => { console.log(e) })
-    } catch (e) {
-      console.log('Error: ', e)
+  const getTokens = async (code, codeVerifier) => {
+
+    const body = {
+      code: code,
+      code_verifier: codeVerifier,
+      redirect_uri: request.redirectUri
     }
+
+    axios.get('http://localhost:5001/betterwrapped/us-central1/app/getTokens', { params: body })
+      .then(res => {
+        const { access_token, refresh_token, expires_in } = res.data;
+        setAccessToken(access_token);
+        setRefreshToken(refresh_token);
+        console.log('Got first access token: ', access_token)
+      })
+      .catch(e => console.log(e))
   }
 
+  const getRefreshedToken = async () => {
 
-  // const getTokens = async () => {
-  //   try {
-  //     const authorizationCode = await getAuthorizationCode()
-  //     const credsB64 = Buffer.from(creds.clientId + ":" + creds.clientSecret).toString('base64');
-  //     const response = await fetch('https://accounts.spotify.com/api/token', {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: `Basic ${credsB64}`,
-  //         'Content-Type': 'application/x-www-form-urlencoded',
-  //       },
-  //       body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${creds.redirectUri
-  //         }`,
-  //     });
-  //     const responseJson = await response.json();
-  //     // destructure the response and rename the properties to be in camelCase to satisfy my linter ;)
-  //     const {
-  //       access_token: accessToken,
-  //       refresh_token: refreshToken,
-  //       expires_in: expiresIn,
-  //     } = responseJson;
+    const body = {
+      refresh_token: refreshToken
+    }
 
-  //     const expirationTime = new Date().getTime() + expiresIn * 1000;
-  //     // await setUserData('accessToken', accessToken);
-  //     // await setUserData('refreshToken', refreshToken);
-  //     // await setUserData('expirationTime', expirationTime);
-  //     console.log('accessToken: ', accessToken, "\nrefreshToken: ", refreshToken, "\nexpirationTime: ", expirationTime)
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
-
+    axios.get('http://localhost:5001/betterwrapped/us-central1/app/getRefreshedToken', { params: body })
+      .then(res => {
+        const { access_token, refresh_token, expires_in } = res.data;
+        setAccessToken(access_token);
+        setRefreshToken(refresh_token);
+        console.log('Got new access token: ', access_token)
+      })
+      .catch(e => { console.log(e) })
+  }
 
   return (
     <View style={styles.container}>
